bc5e0371db804c50b02044ea2928df6725101223
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2026-01-12 11:59:48 -0600

    feat: add basic router setup for /album and /genre

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
+const albumRouter = require("./routes/albumRouter");
+const genreRouter = require("./routes/genreRouter");
+
 const PORT = 3000;
 const APP = "Odin Inventory Application";
 
 app.get("/", (req, res) => {
   res.send("Homepage");
 });
+app.use("/album", albumRouter);
+app.use("/genre", genreRouter);
 
 app.listen(PORT, () => console.log(`${APP}: listening on port ${PORT}`));
