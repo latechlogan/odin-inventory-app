1baa1e83f8341141d7a782683425628d02e941d5
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2026-01-12 17:33:49 -0600

    feat: import controller methods into routers

--- routes/albumsRouter.js
+++ routes/albumsRouter.js
@@ -1,14 +1,14 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", (req, res) => res.send("get all albums"));
-router.get("/create", (req, res) => res.send("get new album form"));
-router.post("/create", (req, res) => res.send("post new album form"));
-router.get("/:albumId/edit", (req, res) => res.send("get edit album form"));
-router.post("/:albumId/edit", (req, res) => res.send("post edit album form"));
-router.post("/:albumId/delete", (req, res) =>
-  res.send("post delete album form/action")
-);
-router.get("/:albumId", (req, res) => res.send("get one specific album"));
+const albumsController = require("../controllers/albumsController");
+
+router.get("/", albumsController.list);
+router.get("/create", albumsController.createGet);
+router.post("/create", albumsController.createPost);
+router.get("/:albumId/edit", albumsController.updateGet);
+router.post("/:albumId/edit", albumsController.updatePost);
+router.post("/:albumId/delete", albumsController.destroy);
+router.get("/:albumId", albumsController.show);
 
 module.exports = router;
--- routes/genresRouter.js
+++ routes/genresRouter.js
@@ -1,14 +1,14 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", (req, res) => res.send("get all genres"));
-router.get("/create", (req, res) => res.send("get new genre form"));
-router.post("/create", (req, res) => res.send("post new genre form"));
-router.get("/:genreId/edit", (req, res) => res.send("get edit genre form"));
-router.post("/:genreId/edit", (req, res) => res.send("post edit genre form"));
-router.post("/:genreId/delete", (req, res) =>
-  res.send("post delete genre form/action")
-);
-router.get("/:genreId", (req, res) => res.send("get one specific genre"));
+const genresController = require("../controllers/genresController");
+
+router.get("/", genresController.list);
+router.get("/create", genresController.createGet);
+router.post("/create", genresController.createPost);
+router.get("/:genreId/edit", genresController.updateGet);
+router.post("/:genreId/edit", genresController.updatePost);
+router.post("/:genreId/delete", genresController.destroy);
+router.get("/:genreId", genresController.show);
 
 module.exports = router;
