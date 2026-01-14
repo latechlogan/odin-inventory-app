ddfa6ad1e2b01ac3583bcb54d76b44024953e01f
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2026-01-14 10:01:30 -0600

    feat: All CRUD query functions implemented

--- db/queries.js
+++ db/queries.js
@@ -1,8 +1,124 @@
 const pool = require("./pool");
 
+//artists
+async function deleteArtist(id) {
+  await pool.query("DELETE FROM artists WHERE artist_id = $1", [id]);
+}
+
+//genres
 async function listAllGenres() {
   const { rows } = await pool.query("SELECT genre_name FROM genres");
   return rows;
 }
 
-module.exports = { listAllGenres };
+async function showGenre(id) {
+  const { rows } = await pool.query(
+    "SELECT genre_name FROM genres WHERE genre_id = $1",
+    [id]
+  );
+  return rows[0];
+}
+
+async function deleteGenre(id) {
+  await pool.query("DELETE FROM genres WHERE genre_id = $1", [id]);
+}
+
+//albums
+async function listAllAlbums() {
+  const { rows } = await pool.query("SELECT * FROM albums");
+  return rows;
+}
+
+async function showAlbum(id) {
+  const { rows } = await pool.query(
+    "SELECT * FROM albums WHERE album_id = $1",
+    [id]
+  );
+  return rows[0];
+}
+
+async function createNewAlbum(albumTitle, artistName, genreName, releaseDate) {
+  const artistId = await findOrCreateArtist(artistName);
+  const genreId = await findOrCreateGenre(genreName);
+
+  await pool.query(
+    `INSERT INTO albums (album_title, artist_id, genre_id, release_date)
+      VALUES ($1, $2, $3, $4)`,
+    [albumTitle, artistId, genreId, releaseDate || null]
+  );
+}
+
+async function updateAlbum(
+  albumId,
+  albumTitle,
+  artistName,
+  genreName,
+  releaseDate
+) {
+  const artistId = await findOrCreateArtist(artistName);
+  const genreId = await findOrCreateGenre(genreName);
+
+  await pool.query(
+    `UPDATE albums SET
+      album_title = $1,
+      artist_id = $2,
+      genre_id = $3,
+      release_date = $4
+      WHERE album_id = $5`,
+    [albumTitle, artistId, genreId, releaseDate || null, albumId]
+  );
+}
+
+async function deleteAlbum(id) {
+  await pool.query("DELETE FROM albums WHERE album_id = $1", [id]);
+}
+
+//helpers
+async function findOrCreateGenre(genreName) {
+  const { rows } = await pool.query(
+    "SELECT genre_id FROM genres WHERE genre_name = $1",
+    [genreName]
+  );
+
+  if (rows.length > 0) {
+    return rows[0].genre_id;
+  } else {
+    const { rows: newRows } = await pool.query(
+      "INSERT INTO genres (genre_name) VALUES ($1) RETURNING genre_id",
+      [genreName]
+    );
+    return newRows[0].genre_id;
+  }
+}
+
+async function findOrCreateArtist(artistName) {
+  const { rows } = await pool.query(
+    "SELECT artist_id FROM artists WHERE LOWER(artist_name) = LOWER($1)",
+    [artistName]
+  );
+
+  if (rows.length > 0) {
+    return rows[0].artist_id;
+  } else {
+    const { rows: newRows } = await pool.query(
+      "INSERT INTO artists (artist_name) VALUES ($1) RETURNING artist_id",
+      [artistName]
+    );
+    return newRows[0].artist_id;
+  }
+}
+
+module.exports = {
+  // genres
+  listAllGenres,
+  showGenre,
+  deleteGenre,
+  // albums
+  listAllAlbums,
+  showAlbum,
+  createNewAlbum,
+  updateAlbum,
+  deleteAlbum,
+  // artists
+  deleteArtist,
+};
