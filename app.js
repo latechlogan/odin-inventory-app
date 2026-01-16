3c8c9a3a1d5a6b16fddfcfd92cc4ae21b3b37f00
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2026-01-15 21:14:36 -0600

    feat: implement ejs and initial views with setup

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 const albumsRouter = require("./routes/albumsRouter");
 const genresRouter = require("./routes/genresRouter");
@@ -10,6 +8,12 @@ const indexController = require("./controllers/indexController");
 
 const PORT = 3000;
 const APP = "Odin Inventory Application";
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", indexController.index);
 app.use("/albums", albumsRouter);
